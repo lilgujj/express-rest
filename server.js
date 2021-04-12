126c0db9d43c642b6c82266e07087d8820c7bb33
FIRST_GUSTAV LAST_ANDERSSON <Gustavkarlandersson@hotmail.com>
2021-04-12 17:13:04 +0200

    remove works finally

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
+const { response } = require("express");
 const express = require("express");
 const server = express();
 const port = 3000;
 let movieLibrary = [
     {
-        title: "Deadpool",
-        description: "man in tight red leggings tell jokes and killing people",
-        id: 1,
-        price: 49
-    },
-    {
         title: "Batman",
         description: "man inte dark leather suite fight people",
-        id: 2,
-        price: 39
+        id: 20,
+        price: 39,
     },
+    {
+        title: "Deadpool",
+        description: "man in tight red leggings tell jokes and killing people",
+        id: 10,
+        price: 49,
+    }
 ];
 
 server.use(express.json());
@@ -28,30 +29,25 @@ server.get("/movies/", (req,res) => {
 // Add movie
 server.post("/movies/", (req,res) => {
     movieLibrary.push(req.body)
+    res.json(movieLibrary)
 })
 
 // Delete movie
-server.delete("/movies/", (req,res) => {
-    const index = movieLibrary.findIndex(movie => movie.title === "Batman");
-    const removedMovie = movieLibrary.splice(index,1);
-    res.json(removedMovie)
-
-    // movieLibrary.map(item => {
-    //     // change string to title of the movie u wish to remove
-    //     if(item.title === "Batman"){
-    //         console.log(item)
-    //         const removedMovie = movieLibrary.splice(item, 1)
-    //         res.json(removedMovie)
-    //         return
-    //     } if(item.title === null | undefined) {
-    //         res.status(404).json("No movie found with that name")
-    //     }
-    // })
+server.delete("/movies/:id", (req,res) => {
+    const { id } = req.params;
+    const deletedMovie = movieLibrary.findIndex(item => item.id == id);
+    const movie = movieLibrary.find(item => item.id == id);
+    if (movie) {
+        const deleted = movieLibrary.splice(deletedMovie, 1)
+        return res.json(deleted)
+    } if(!movie) {
+        res.json("selected movie does not exist")
+    }
 })
 
 // change movie
 server.put("/movies/:id", (req,res) => {
-    const { id } = req.params;
+    const id = req.params.id;
     const { title, description, price } = req.body;
     const updatedMovie = movieLibrary.find((movie) => movie.id == id);
 
